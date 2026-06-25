Sparsity curves, new results, gradient & activation distributions, fixed bugs with loss and accuracy curves.

main.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn as nn
+from torch import nn
 import torchvision
 import torchvision.transforms.v2 as v2
 import numpy as np
@@ -23,7 +23,7 @@ def plot_gradient_steps(ax, grad_flows, activation_name, layers):
         clean_name = layer_name.replace('.weight', '')
         
         for trial_idx in range(num_trials):
-            layer_data = grad_flows[trial_idx, layer_idx, :]  
+            layer_data = grad_flows[trial_idx, layer_idx, :]
             layer_data_safe = layer_data + 1e-8
             label_text = clean_name if trial_idx == 0 else None
             ax.plot(steps_range, layer_data_safe, 
@@ -38,14 +38,39 @@ def plot_gradient_steps(ax, grad_flows, activation_name, layers):
     ax.set_yscale('log')  
     ax.legend(fontsize=8, loc='upper right')
 
+def sparsity_curve(activations, avg_sparsity_curve):
+    epochs = range(1, len(avg_sparsity_curve['ReLU'][0]) + 1)
+    for name in activations:
+        plt.figure(figsize=(10, 5))
+        arr = np.array(avg_sparsity_curve[name])
+        mean_curve = np.mean(arr, axis=0)
+        std_curve = np.std(arr, axis=0)
+        for i in range(mean_curve.shape[1]):
+            plt.plot(epochs, mean_curve[:, i], label=f"{i}")
+            plt.fill_between(
+                epochs,
+                mean_curve[:, i] - std_curve[:, i],
+                mean_curve[:, i] + std_curve[:, i],
+                alpha=0.2
+            )
+        plt.xlabel("Epoch")
+        plt.ylabel("Sparsity" if name == 'ReLU' else "Activation Saturation Rate")
+        plt.title(f"{name} - {'Sparsity' if name == 'ReLU' else 'Activation Saturation Rate'} Curve (Mean and Std over Trials)")
+        plt.legend()
+        plt.grid(alpha=0.3)
+        plt.tight_layout()
+        plt.savefig(f'{name}_sparsity_curve.pdf', bbox_inches='tight')
+
+
 def plot_curves(activations, avg_accuracy_curve, avg_loss_curve):
     fig, ax = plt.subplots(1, 2, figsize=(14, 5))
-    epochs = range(1, len(avg_accuracy_curve[list(activations.keys())[0]]) + 1)
+    epochs = range(1, len(avg_accuracy_curve['ReLU'][0]) + 1)
+    print(epochs)
     for name in activations:
         arr = np.array(avg_accuracy_curve[name])
         mean_curve = np.mean(arr, axis=0)
+        print(mean_curve)
         std_curve = np.std(arr, axis=0)
-
         ax[0].plot(epochs, mean_curve, label=name)
         ax[0].fill_between(
             epochs,
@@ -61,10 +86,8 @@ def plot_curves(activations, avg_accuracy_curve, avg_loss_curve):
 
     for name in activations.keys():
         arr = np.array(avg_loss_curve[name])
-
         mean_curve = np.mean(arr, axis=0)
         std_curve = np.std(arr, axis=0)
-
         ax[1].plot(epochs, mean_curve, label=name)
         ax[1].fill_between(
             epochs,
@@ -81,25 +104,58 @@ def plot_curves(activations, avg_accuracy_curve, avg_loss_curve):
     plt.show()
     fig.savefig('accuracy_loss_curves.pdf', bbox_inches='tight')
 
+def plot_distributions(model, loader, device):
+    fig, ax = plt.subplots(1, 2, figsize=(20, 4))
+    activation_name = model.activation.__class__.__name__
+    model.eval()
+    images, _ = next(iter(loader))
+    images = images.to(device)
+    labels = model(images)
+    loss = labels.sum()
+    model.zero_grad()
+    loss.backward()
+
+    legends = []
+    for name, out in model.saved.items():
+        if out.grad is None:
+            continue
+        t = out.grad.detach().cpu().flatten()
+        print(f"{activation_name} - {name} grad mean {t.mean():.4f} std {t.std():.4f}")
+        hy, hx = torch.histogram(t, bins=50, density=True)
+        ax[0].plot(hx[:-1], hy)
+        legends.append(name)
+    ax[0].legend(legends)
+    ax[0].set_title(f"Gradient distributions - {activation_name}")
+
+    legends = []
+    for name, out in model.saved.items():
+        t = out.detach().cpu().flatten()
+        print(f"{activation_name} - {name} act mean {t.mean():.4f} std {t.std():.4f}")
+
+        hy, hx = torch.histogram(t, bins=50, density=True)
+        ax[1].plot(hx[:-1], hy)
+        legends.append(name)
+    ax[1].legend(legends)
+    ax[1].set_title(f"Activation distributions - {activation_name}")
+    fig.savefig(f'{activation_name}_distributions.pdf', bbox_inches='tight')
+
 def main():
     experiment_mode = False
     train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
     test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True, num_workers=2)
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=32, shuffle=False, num_workers=2)
-
     
     activations = {
-        'Tanh': nn.Tanh(),
         'ReLU': nn.ReLU(),
-        'GELU': nn.GELU()
-
+        'Tanh': nn.Tanh(),
     }
     final_test_loss = {activation: [] for activation in activations}
     final_accuracy = {activation: [] for activation in activations}
     avg_accuracy_curve = {activation: [] for activation in activations}
     avg_loss_curve = {activation: [] for activation in activations}
     avg_grad_flows = {activation: [] for activation in activations}
+    avg_sparsity_curve = {activation: [] for activation in activations}
     
     epochs = 2 if experiment_mode else 15
     seeds = [1, 2] if experiment_mode else range(1, 6)
@@ -112,19 +168,24 @@ def main():
         for name, activation in activations.items():
             print(f'--- Training {name} model ---')
             model = CNN(activation)
-            test_loss, acc, grad_flows = train(model, train_loader, test_loader, device, epochs=epochs)
+            model = model.to(device)
+            if seed == seeds[-1]:
+                plot_distributions(model, train_loader, device)
+            sparsity, test_loss, acc, grad_flows = train(model, train_loader, test_loader, device, epochs=epochs)
+
             final_test_loss[name].append(test_loss[-1])
             final_accuracy[name].append(acc[-1])
             avg_loss_curve[name].append(test_loss)
             avg_accuracy_curve[name].append(acc)
             avg_grad_flows[name].append(grad_flows)
+            avg_sparsity_curve[name].append(sparsity)
 
     print("########################## Final Results ##########################\n\n")
     for name in activations:
         print(name)
         print(f'{name}: Test Loss: {final_test_loss[name]} | Accuracy: {final_accuracy[name]}\n')
 
-    fig, ax = plt.subplots(1, 3, figsize=(21, 6))
+    fig, ax = plt.subplots(1, len(activations), figsize=(21, 6))
     for i, name in enumerate(activations):
         grad_trial = []
         for trial in avg_grad_flows[name]:
@@ -137,5 +198,6 @@ def main():
     fig.savefig('gradient_curves.pdf', bbox_inches='tight')
     plt.show()
     plot_curves(activations, avg_accuracy_curve, avg_loss_curve)
+    sparsity_curve(activations, avg_sparsity_curve)
 
 if __name__ == '__main__': main()

model.py
@@ -1,34 +1,50 @@
 import torch.nn as nn
 
 class CNN(nn.Module):
-  def __init__(self, activation, n_hidden=128):
+  def __init__(self, activation, hidden=128):
     super().__init__()
 
-    self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-    self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
-    self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-    self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-    self.conv5 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-    self.conv6 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+    self.saved = {}
+    self.activation = activation
+    self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+    self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
+    self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
     self.pool = nn.MaxPool2d(2, 2)
 
-    self.activation = activation
     self.flatten = nn.Flatten()
 
-    self.fc1 = nn.Linear(128 * 8 * 8, n_hidden)
-    self.fc2 = nn.Linear(n_hidden, 10)
+    self.fc1 = nn.Linear(64 * 16 * 16, hidden)
+    self.fc2 = nn.Linear(hidden, 10)
 
+    for m in self.modules():
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+            nn.init.kaiming_normal_(m.weight)
+            nn.init.zeros_(m.bias)
+
+  def save_act(self, name, x):
+    if x.requires_grad:
+      x.retain_grad()
+    self.saved[name] = x
   def forward(self, x):
-    x = self.activation(self.conv1(x))
-    x = self.pool(self.activation(self.conv2(x)))
-    x = self.activation(self.conv3(x))
-    x = self.pool(self.activation(self.conv4(x)))
-    x = self.activation(self.conv5(x))
-    x = self.activation(self.conv6(x))
+    x = self.conv1(x)
+    x = self.activation(x)
+    self.save_act('conv1', x)
+
+    x = self.conv2(x)
+    x = self.activation(x)
+    self.save_act('conv2', x)
+
+    x = self.pool(x)
+
+    x = self.conv3(x)
+    x = self.activation(x)
+    self.save_act('conv3', x)
 
     x = self.flatten(x)
 
-    x = self.activation(self.fc1(x))
+    x = self.fc1(x)
+    x = self.activation(x)
+    self.save_act('fc1', x)
+    
     x = self.fc2(x)
-
     return x

training.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
 
 
 def train(model, train_loader, test_loader, device, epochs=15):
@@ -12,10 +13,11 @@ def train(model, train_loader, test_loader, device, epochs=15):
         if 'weight' in name:
             grad_flows[name] = []
 
+    sparsities = []
     for epoch in range(epochs):
         model.train()
         running_loss = 0.0
-        for images, labels in train_loader:
+        for step_idx, (images, labels) in enumerate(train_loader):
             images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = model(images)
@@ -27,6 +29,7 @@ def train(model, train_loader, test_loader, device, epochs=15):
                     grad_flows[name].append(param.grad.norm().item())
             optimizer.step()
             running_loss += loss.item()
+        sparsities.append(sparsity(model))
         train_loss = running_loss / len(train_loader)
         train_losses.append(train_loss)
         test_loss, accuracy = evaluate(model, test_loader, device)
@@ -34,7 +37,13 @@ def train(model, train_loader, test_loader, device, epochs=15):
         test_accuracies.append(accuracy)
 
         print(f"Epoch {epoch+1}/{epochs} | Train Loss: {train_loss:.4f} | Test Loss: {test_loss:.4f} | Acc: {accuracy:.4f}")
-    return test_losses, test_accuracies, grad_flows
+    return sparsities, test_losses, test_accuracies, grad_flows
+
+# trials x epochs x layers (avg over trials)
+def sparsity(model):
+    activation = model.activation.__class__.__name__
+    sparsities = [(out.detach().cpu() < 0.01).float().mean().item() for out in model.saved.values()] if activation == 'ReLU' else [(out.detach().cpu().abs() > 0.97).float().mean().item() for out in model.saved.values()]
+    return sparsities
 
 def evaluate(model, data_loader, device):
     model.eval()